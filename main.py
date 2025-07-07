8df92c7f9fb0a7b0354645db95a8877e6a43ddef
chirayu <chirayujain93@gmail.com>
2025-07-08 01:01:48 +0530

    updated main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from app import create_app, db
 app = create_app()
 
 if __name__ == '__main__':
+    
     with app.app_context():
-        db.create_all()  # Create database tables
+        db.create_all()  
     app.run(debug=True)  
